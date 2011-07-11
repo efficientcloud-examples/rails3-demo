441e2761e901f4e100d0364b9e2c6621c6bea755
Christian Hofstaedtler <c.hofstaedtler@efficientcloud.com>
2011-07-11 18:53:50 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = '856f17c33d5d56ae46000bdee5603cce606b8bc40c8c4f88dbc6c8a7de0b2fe431ff197b376acaf22d589810c0df113a81cb566d0e4b640164dfef14e602ece0'
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Daley', :city => cities.first)
+
+p = Post.new(:content => "A new post", :name => "apost", :title => "A new post title!")
+p.save!
+
